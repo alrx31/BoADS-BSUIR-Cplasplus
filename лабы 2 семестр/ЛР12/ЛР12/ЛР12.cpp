d8b7cf24102ca1445545ec8ba130ea8ada79b276
Usov <usov.lesha2017@gmail.com>
2024-03-05 14:56:48 +0300

    Solve the LT 12

--- лабы 2 семестр/ЛР12/ЛР12/ЛР12.cpp
+++ лабы 2 семестр/ЛР12/ЛР12/ЛР12.cpp
@@ -1,109 +1,117 @@
 ﻿#include <iostream>
 
 using namespace std;
-template <typename T>
-class Stack {
-	int size;
-	T* arr;
-	
-public:
-	Stack() {
-		size = 0;
-		T* arr = new T[size+1];
-	}
 
-	void add(T data) {
-		T* temp = new T[size + 1];
-		for (int i = 0; i < size; i++) {
-			temp[i] = arr[i];
-		}
-		temp[size] = data;
-		delete[] arr;
-		arr = temp;
-		size++;
-	}
-
-	T pop() {
-		return arr[--size];
-	}
-
-
-};
 
 template <typename T>
 class Queue {
-	
-    int size;
-	T* arr;
+	class Node {
+		T data;
+		Node* next;
+	public:
+		Node(T data, Node* next = nullptr) : data(data), next(next) {}
+		T getData() { return data; }
+		Node* getNext() { return next; }
+		void setNext(Node* next) { this->next = next; }
+		void setData(T data) { this->data = data; }
+	};
+
+	Node* head;
+	Node* tail;
+	int size;
 public:
-	Queue() {
-		size = 0;
-		T* arr = new T[size + 1];
-	}
-	void add(T data) {
-		T* temp = new T[size + 1];
-		for (int i = 0; i < size; i++) {
-			temp[i] = arr[i];
+	Queue() : head(nullptr), tail(nullptr), size(0) {}
+	~Queue() {
+		while (head) {
+			Node* temp = head;
+			head = head->getNext();
+			delete temp;
 		}
-		temp[size] = data;
-		delete[] arr;
-		arr = temp;
+	}
+	void push(T data) {
+		if (head == nullptr) {
+			head = tail = new Node(data);
+		}
+		else {
+			tail->setNext(new Node(data));
+			tail = tail->getNext();
+		}
 		size++;
 	}
 	T pop() {
-		T* temp = new T[size - 1];
-		for (int i = 0; i < size - 1; i++) {
-			temp[i] = arr[i + 1];
+		if (head == nullptr) {
+			throw "Queue is empty";
 		}
-		T res = arr[0];
-		delete[] arr;
-		arr = temp;
+		T data = head->getData();
+		Node* temp = head;
+		head = head->getNext();
+		delete temp;
 		size--;
-		return res;
+		return data;
 	}
-
+	T front() {
+		if (head == nullptr) {
+			throw "Queue is empty";
+		}
+		return head->getData();
+	}
+	T shift() {
+		if (tail == nullptr) {
+			throw "Queue is empty";
+		}
+		return tail->getData();
+		
+	}
 	int getSize() {
 		return size;
 	}
-
-};
-
-class Task {
-	int type;
-	int difficulty;
-
-public:
-	Tack(int Type, int Difficulty) 
-	{
-		this->type = Type;
-		this->difficulty = Difficulty;
-	}
-	int getType() {
-		return this->type;
-	}
-	int getDifficulty() {
-		return this->difficulty;
+	bool isEmpty() {
+		return size == 0;
 	}
 };
-
 
 
 int main() {
+
+	int* arr = new int[10];
+
+	for (int i = 1; i < 11; i++) {
+		arr[i-1] = i;
+	}
+
+	Queue<int > odd;
+	Queue<int > even;
+
+	for (int i = 0; i < 10; i++) {
+		if ((i + 1)% 2 == 0) {
+			even.push(arr[i]);
+		}
+		else {
+			odd.push(arr[i]);
+		}
+	}
+
+
+
+	cout << "Odd queue: " << endl;
+	cout << "Head: " << odd.front() << endl;
+	cout << "Tail: " << odd.shift() << endl;
+	size_t size1 = odd	.getSize();
+	for (int i = 0; i < size1; i++) {
+		cout << odd.pop() << " ";
+	}
 	
-	Queue<Task> queue;
-	Stack<Task> stack;
+	cout << endl;
 	
-
-	queue.add(Task(1, 1));
-	queue.add(Task(1, 2));
-	queue.add(Task(2, 2));
-	queue.add(Task(2, 3));
-	queue.add(Task(3, 3));
-	queue.add(Task(3, 4));
-
-
 	
-
+	cout << "Even queue: " << endl;
+	cout << "Head: " << even.front() << endl;
+	cout << "Tail: " << even.shift() << endl;
+	size_t size2 = even.getSize();
+	for (int i = 0; i < size2; i++) {
+		cout << even.pop() << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
