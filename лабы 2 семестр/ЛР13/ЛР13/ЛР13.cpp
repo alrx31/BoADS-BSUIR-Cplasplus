757beaf41fd9a19acc2cf1fb4dd1cffce35d20eb
Usov <usov.lesha2017@gmail.com>
2024-03-05 17:59:32 +0300

    Solve the LT13 with the report

--- лабы 2 семестр/ЛР13/ЛР13/ЛР13.cpp
+++ лабы 2 семестр/ЛР13/ЛР13/ЛР13.cpp
@@ -1,20 +1,164 @@
-﻿// ЛР13.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+using namespace std;
 
-#include <iostream>
+template <typename T>
+class Stack {
+	class Node {
+		T data;
+		Node* prev;
+	public:
+		Node(): prev(nullptr) {}
+		Node(T data, Node* node): data (data), prev(node) {}
+		T getData() { return data; }
+		Node* getPrev() { return prev; }
+		void setPrev(Node* prev) { this->prev = prev; }
+		void setData(T data) { this->data = data; }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	};
+	Node* tail;
+	int size;
+public:
+	Stack(): tail(nullptr), size(0) {}
+	void push(T data){
+		if (tail == nullptr) {
+			tail = new Node(data,nullptr);
+		}
+		else {
+			Node* newNode = new Node(data,tail);
+			tail = newNode;
+		}
+		size++;
+	}
+	T pop() {
+		if (size == 0) {
+			throw "Stack is empty";
+		}
+		T data = tail->getData();
+		tail = tail->getPrev();
+		size--;
+		return data;
+	}
+
+	T peek() {
+		if (size == 0) {
+			throw "Stack is empty";
+		}
+		return tail->getData();
+	}
+
+	bool isEmpty() {
+		return size == 0;
+	}
+
+};
+
+
+
+float solveOPZ(string expression) {
+	Stack<float> stack;
+	for (int i = 0; i < expression.size(); i++) {
+		if (expression[i] == ' ') {
+			continue;
+		}
+		int x = expression[i] -'0';
+		if (expression[i] >= '0' && expression[i] <= '9') {
+			float number = 0;
+			while (expression[i] != ' ' && i < expression.size()) {
+				number = number * 10 + (expression[i] - '0');
+				i++;
+			}
+			stack.push(number);
+		}
+		else {
+			float a = stack.pop();
+			float b = stack.pop();
+			switch (expression[i]) {
+			case '+': {
+					stack.push(a + b);
+					break;
+				}
+			case '-': {
+					stack.push(b - a);
+					break;
+				}
+			case '*': {
+					stack.push(a * b);
+					break;
+				}
+			case '/': {
+				if (a == 0) {
+						throw "Division by zero";
+					}
+					stack.push(b/ a);
+					break;
+				}
+			}
+		}
+
+
+	}
+	if (stack.isEmpty()) {
+		throw "Invalid expression";
+		return -1;
+	}
+	else {
+		return stack.pop();
+	}
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+
+int main() {
+
+	string expression1 = "3 1 +";
+	string expression2 = "12 5 * 10 -";
+	string expression3 = "1 2 30 + *";
+	string expression4 = "2 10 + 2 4 + 6 – 2 /";
+
+
+	cout << solveOPZ(expression1) << endl;
+	cout << solveOPZ(expression2) << endl;
+	cout << solveOPZ(expression3) << endl;
+	cout << solveOPZ(expression4) << endl;
+
+	if (solveOPZ(expression1) == 4) {
+		cout << "Test 1 passed" << endl;
+	}
+	else {
+		cout << "Test 1 failed" << endl;
+	}
+
+	if (solveOPZ(expression2) == 50) {
+		cout << "Test 2 passed" << endl;
+	}
+	else {
+		cout << "Test 2 failed" << endl;
+	}
+		
+	if (solveOPZ(expression3) == 32) {
+		cout << "Test 3 passed" << endl;
+	}
+	else {
+		cout << "Test 3 failed" << endl;
+	}
+
+	if (solveOPZ(expression4) == 6) {
+		cout << "Test 4 passed" << endl;
+	}
+	else {
+		cout << "Test 4 failed" << endl;
+	}
+
+
+	string expression;
+	cout << "Enter expression in reverse polish notation: " << endl;
+	cout << " use this format: 3 1 + " << endl;
+	getline(cin, expression);
+	cout << "Result : " << solveOPZ(expression) << endl;
+
+
+
+	return 0;
+}
